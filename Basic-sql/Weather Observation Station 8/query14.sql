54ce9d6396c47bb9520115bbaaa1873d0e2a2d01
Baktho SN <aa6417796@gmail.com>
2023-01-13 14:32:22 +0530

    Basic sql query

---
+++ Basic-sql/Weather Observation Station 8/query14.sql
@@ -0,0 +1,13 @@
+SELECET 
+    DISTINCT CITY 
+FROM 
+    STATION 
+WHERE 
+    SUBSTR(LOWER(CITY),1,1) 
+IN 
+    ('a','e','i','o','u') 
+AND 
+    SUBSTR(LOWER(CITY),LENGTH(CITY),1)
+IN 
+    ('a','e','i','o','u');
+    
